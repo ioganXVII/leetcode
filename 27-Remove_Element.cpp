010f611c15c84c5cbf865d860829758a83747a4c
Artem <asustransformertf300x@gmail.com>
2021-06-13 18:28:18 +0300

    Create 27-Remove_Element.cpp

---
+++ 27-Remove_Element.cpp
@@ -0,0 +1,10 @@
+int removeElement(vector<int>& nums, int val) {
+        int result = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != val){
+                nums[result] = nums[i];
+                result++;
+            }
+        }
+        return result;
+    }

eca28ecdc093526f1be843d91ca7e75508e37071
Artem <asustransformertf300x@gmail.com>
2021-07-30 05:36:20 +0300

    Create 70-Climbing_Stairs.cpp
    Using Fibonacci Numbers.

---
+++ 70-Climbing_Stairs.cpp
@@ -0,0 +1,15 @@
+// Using Fibonacci Numbers.
+int climbStairs(int n) {
+        if(n == 1) return 1;
+        
+        int first = 1;
+        int second = 2;
+        
+        for(int i = 3; i <= n; i++){
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        
+        return second;
+    }

85142e7614080ce2ded166ca110fadd1f4136c78
Artem <asustransformertf300x@gmail.com>
2021-08-03 09:15:03 +0300

    Create 338-Counting_Bits.cpp

---
+++ 338-Counting_Bits.cpp
@@ -0,0 +1,8 @@
+vector<int> countBits(int n) {
+        vector<int> result(n + 1, 0);
+        for(int i = 0; i <= n; i++){
+            result[i] = result[i >> 1] + i % 2;
+        }
+        
+        return result;
+    }
